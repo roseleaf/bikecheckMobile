52b7fe40819bad514bddd464d447fc78c26fcb79
roseleaf <pteroline@gmail.com>
2012-11-21 22:39:47 -0800

    Initial Commit

---
+++ BikeCheck/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BikeCheck
+//
+//  Created by Rose CW on 11/21/12.
+//  Copyright (c) 2012 Rose Trujillo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
